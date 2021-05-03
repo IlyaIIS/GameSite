8e9465e63e3da2916eace807e417def658d7bffb
Degener8 <raptoroidinbox@gmail.com>
2021-05-03 08:51:02 +0500

    node+express+ejs

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use('/public', express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+
+app.listen(PORT, () => {
+  console.log('Server has been started...');
+});
